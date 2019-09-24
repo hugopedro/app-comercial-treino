48c17ace09a8a7018f8fac654dab9bb0e1c5ada6
hugopedro <hugopedro098@gmail.com>
2019-09-24 20:33:07 -0300

    Other entities and second migration v2

--- AppComercial/Migrations/20190924232936_OtherEntities.Designer.cs
+++ AppComercial/Migrations/20190924232936_OtherEntities.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using AppComercial.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AppComercial.Migrations
 {
     [DbContext(typeof(AppComercialContext))]
-    partial class AppComercialContextModelSnapshot : ModelSnapshot
+    [Migration("20190924232936_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
