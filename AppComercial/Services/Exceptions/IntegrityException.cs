76e0ecdfdaadaff466310f4ffc3a66a7eb7f48a6
hugopedro <hugopedro098@gmail.com>
2019-09-27 21:20:43 -0300

    Exception handling for delete (referential integrity)

---
+++ AppComercial/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppComercial.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException //herdou do ApplicationException
+    { //essa vai ser a exceção personalizada de SERVIÇO para erros de integridade referencial(ex:atributo preso à chave estrangeira)
+        public IntegrityException(string message) : base(message)
+        {
+            //serve pra repassar a chamada pra superclasse
+        }
+    }
+}
