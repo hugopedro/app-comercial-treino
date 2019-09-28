334579345c02a2bad87bae9fcb015d287582c620
hugopedro <hugopedro098@gmail.com>
2019-09-28 11:27:11 -0300

    Implementing simple and group search, and finalization

--- AppComercial/Controllers/SalesRecordsController.cs
+++ AppComercial/Controllers/SalesRecordsController.cs
@@ -3,24 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AppComercial.Services;
 
 namespace AppComercial.Controllers
 {
     public class SalesRecordsController : Controller //na hora de criar o controlador deve-se obedecer o nome da classe + o s(plural)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }   
+        //declarando a dependencia
+        private readonly SalesRecordService _salesRecordService;
 
-        public IActionResult SimpleSearch()
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+            _salesRecordService = salesRecordService;
+        }
+
+        public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult GroupingSearch()
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
+            return View(result);
+        }
+
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
+        {
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            // isso é pras datas ficarem armazenadas na caixinha, o controlador envia à view
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _salesRecordService.FindByDateGroupingAsync(minDate, maxDate);
+            return View(result);
         }
     }
 }
---
+++ AppComercial/Services/SalesRecordService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppComercial.Models;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AppComercial.Services
+{
+    public class SalesRecordService
+    {
+        private readonly AppComercialContext _context;
+
+        public SalesRecordService(AppComercialContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        { // o ? é pq é opcional ter data minima e maxima
+            var result = from obj in _context.SalesRecord select obj; //essa consulta não é executada pela simples definição dela
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj; //essa consulta não é executada pela simples definição dela
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date) // pra rodar o group by tem q por Task<List<IGrouping<Department,SalesRecord>
+                .GroupBy(x => x.Seller.Department) //isso agrupa os resultados usando linq
+                .ToListAsync();
+        }
+    }
+}
